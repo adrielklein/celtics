b9462c6a4fa30ed3a118723bf1d5973ebde66cf5
Adriel Klein <adriel@pillar.app>
2020-04-10 13:49:06 -0400

    make it boston celtics style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import data from "./data";
 
-function App() {
+const Image = ({ imageUrl }) => (
+  <div
+    style={{
+      margin: "5px",
+      border: "1px solid #ccc",
+      width: "180px"
+    }}
+  >
+    <a target="_blank" href={imageUrl}>
+      <img src={imageUrl} width="500" height="282" />
+    </a>
+  </div>
+);
+
+const App = () => {
+  const teamData = data.teams[0];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "50px" }}>
+      <div>
+        <img
+          style={{ height: "100px", width: "100px", display: "inline-block" }}
+          src={teamData.strTeamBadge}
+          alt="team image"
+        />
+        <div style={{ display: "inline-block", fontSize: "50px" }}>
+          <p style={{ fontSize: "50px" }}>{teamData.strTeam}</p>
+        </div>
+        <img
+          style={{ height: "100px", width: "100px", display: "inline-block" }}
+          src={teamData.strTeamBadge}
+          alt="team image"
+        />
+      </div>
+      <p>{teamData.strDescriptionEN}</p>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "500px 500px",
+          columnGap: "10px"
+        }}
+      >
+        <Image imageUrl={teamData.strTeamFanart1} />
+        <Image imageUrl={teamData.strTeamFanart2} />
+        <Image imageUrl={teamData.strTeamFanart3} />
+        <Image imageUrl={teamData.strTeamFanart4} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
